5bb8811123c9c894e24315a8c6076fa6fa2d550e
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-09 07:02:28 -0300

    Refatorando para as novas funcionalidades

--- script.js
+++ script.js
@@ -8,9 +8,28 @@ const buttonSoundOff = document.querySelector('.sound-off')
 buttonPlay.onclick = () => {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
+  buttonSet.classList.add('hide')
+  buttonStop.classList.remove('hide')
 }
 
 buttonPause.onclick = () => {
   buttonPause.classList.add('hide')
   buttonPlay.classList.remove('hide')
+}
+
+buttonStop.onclick = () => {
+  buttonStop.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonPlay.classList.remove('hide')
+}
+
+buttonSoundOn.onclick = () => {
+  buttonSoundOn.classList.add('hide')
+  buttonSoundOff.classList.remove('hide')
+}
+
+buttonSoundOff.onclick = () => {
+  buttonSoundOff.classList.add('hide')
+  buttonSoundOn.classList.remove('hide')
 }

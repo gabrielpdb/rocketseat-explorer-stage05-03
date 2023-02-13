56260041f561ff07ef5cfd5936185111f6eee84d
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-12 22:50:03 -0300

    Criando objetos com o padrão Factory e injeção de dependências

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import resetControls from './controls.js'
-import { countdown, resetTimer } from './timer.js'
+import { Timer } from './timer.js'
 
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
@@ -12,13 +12,21 @@ const secondsDisplay = document.querySelector('.seconds')
 let minutes = Number(minutesDisplay.textContent)
 let timerTimeOut
 
+const configTimer = {
+  minutesDisplay,
+  secondsDisplay,
+  timerTimeOut,
+  resetControls
+}
+const timer = Timer(configTimer)
+
 buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
 
-  countdown()
+  timer.countdown()
 })
 
 buttonPause.addEventListener('click', function () {
@@ -29,7 +37,7 @@ buttonPause.addEventListener('click', function () {
 
 buttonStop.addEventListener('click', function () {
   resetControls()
-  resetTimer()
+  timer.resetTimer()
 })
 
 buttonSoundOff.addEventListener('click', function () {
@@ -45,7 +53,7 @@ buttonSoundOn.addEventListener('click', function () {
 buttonSet.addEventListener('click', function () {
   let newMinutes = prompt('Quantos minutos?')
   if (!newMinutes) {
-    resetTimer()
+    timer.resetTimer()
     return
   }
 

27d00f883422bcad6b43078d3f94b7fdc546b0af
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-13 21:14:40 -0300

    done

---
+++ js/events.js
@@ -0,0 +1,56 @@
+import { elements } from './elements.js'
+
+const {
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  buttonSet,
+  buttonSoundOn,
+  buttonSoundOff
+} = elements
+
+export default function Events({ controls, timer, sound }) {
+  buttonPlay.addEventListener('click', function () {
+    controls.play()
+    timer.countdown()
+    sound.pressButton()
+  })
+
+  buttonPause.addEventListener('click', function () {
+    controls.pause()
+    timer.hold()
+    sound.pressButton()
+  })
+
+  buttonStop.addEventListener('click', function () {
+    controls.reset()
+    timer.reset()
+    sound.pressButton()
+  })
+
+  buttonSoundOff.addEventListener('click', function () {
+    buttonSoundOn.classList.remove('hide')
+    buttonSoundOff.classList.add('hide')
+    sound.bgAudio.pause()
+  })
+
+  buttonSoundOn.addEventListener('click', function () {
+    buttonSoundOn.classList.add('hide')
+    buttonSoundOff.classList.remove('hide')
+    sound.bgAudio.play()
+  })
+
+  buttonSet.addEventListener('click', function () {
+    let newMinutes = controls.getMinutes()
+
+    if (!newMinutes) {
+      timer.reset()
+      return
+    }
+
+    timer.updateDisplay(newMinutes, 0)
+    timer.updateMinutes(newMinutes)
+  })
+
+  return {}
+}
